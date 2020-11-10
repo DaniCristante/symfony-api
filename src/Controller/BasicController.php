c7931e0cbac318ae13a5b403f25e157759841500
Daniel <daniel@runroom.com>
2020-11-11 00:26:53 +0100

    basic response test

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -7,15 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route(path="/api/")
+ */
 class BasicController extends AbstractController
 {
     /**
-     * @Route("/basic", name="basic")
+     * @Route("basic", name="basic", METHODS={"GET"})
      */
     public function test(): JsonResponse
     {
         return new JsonResponse([
             'status' => 'Hola!' 
-        ], 200);
+        ]);
     }
 }
