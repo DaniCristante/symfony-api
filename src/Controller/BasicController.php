c2d6a6604181418c9e6b5dbf85817b0e6171b1f7
Daniel <daniel@runroom.com>
2020-11-11 18:15:26 +0100

    wip basic api routing

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -7,14 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route(path="/api/")
- */
+
 class BasicController extends AbstractController
 {
-    /**
-     * @Route("basic", name="basic", METHODS={"GET"})
-     */
     public function test(): JsonResponse
     {
         return new JsonResponse([
